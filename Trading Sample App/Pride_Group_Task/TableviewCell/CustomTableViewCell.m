b88a12d40253e42c6f22aaf3a79aa45e4ddb3f6b
Ajesh003 <ajesh.r1@gmail.com>
2023-02-17 21:23:12 +0400

    Add files via upload

---
+++ Trading Sample App/Pride_Group_Task/TableviewCell/CustomTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.m
+//  Pride_Group_Task
+//
+//  Created by OLIVE on 15/02/2023.
+//
+
+#import "CustomTableViewCell.h"
+
+@implementation CustomTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
