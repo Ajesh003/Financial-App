dca74b705776f80ac20c7ae826e76794deaa7fab
Ajesh003 <ajesh.r1@gmail.com>
2023-02-17 20:41:50 +0400

    Add files via upload

---
+++ Pride_Group_Task/Pride_Group_Task/Delegates/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Pride_Group_Task
+//
+//  Created by OLIVE on 15/02/2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Pride_Group_Task/Pride_Group_Task/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Pride_Group_Task
+//
+//  Created by OLIVE on 15/02/2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
